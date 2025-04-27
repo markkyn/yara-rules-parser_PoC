13e0a336b0479661d3ec4e46d609ad79b41c2bef
Markkyn <marcos.gabriel@ossirian.com>
2025-04-26 23:34:56 -0300

    new: ProcessBuilder to call yara

--- src/main/java/com/politec/markkyn/Main.java
+++ src/main/java/com/politec/markkyn/Main.java
@@ -1,8 +1,29 @@
 package com.politec.markkyn;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.io.File;
+
+
 public class Main {
     public static void main(String[] args) {
+        File malwareFolder =  new File("/malwares");
+
+        ProcessBuilder builder = new ProcessBuilder("./bin/yara.exe");
+
+        try {
+            builder.start();
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void listAllFiles(final File folder) {
+        for (final File fileEntry : folder.listFiles()) {
+            if (fileEntry.isDirectory()) {
+                listAllFiles(fileEntry);
+            }
+
+
+        }
     }
 }
